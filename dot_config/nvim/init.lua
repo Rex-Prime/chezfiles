1db41e74aaae1510bfce74748664cffec36a3472
Rex-Prime <rexdotprime@gmail.com>
2026-07-20 08:42:11 -0400

    feat(cord.nvim): add plugin + config

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -977,6 +977,7 @@ do
   --
   --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
   -- require 'custom.plugins'
+  require 'custom.plugins.cord'
 end
 
 -- The line beneath this is called `modeline`. See `:help modeline`
