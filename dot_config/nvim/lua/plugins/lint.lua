37fcf0d88432c97b45938d1e6c94c8a59f74ca4c
Rex-Prime <rexdotprime@gmail.com>
2026-08-09 03:53:02 -0400

    feat(nvim): add stylua as linter
    - formatted file

--- dot_config/nvim/lua/plugins/lint.lua
+++ dot_config/nvim/lua/plugins/lint.lua
@@ -1,14 +1,15 @@
 return {
-  "mfussenegger/nvim-lint",
+  'mfussenegger/nvim-lint',
 
   config = function()
-    local lint = require("lint")
+    local lint = require('lint')
 
     lint.linters_by_ft = {
-      markdown = { "markdownlint" },
+      markdown = { 'markdownlint-cli2' },
+      lua = { 'stylua' },
     }
 
-    vim.api.nvim_create_autocmd({ "BufWritePost", "BufEnter" }, {
+    vim.api.nvim_create_autocmd({ 'BufWritePost', 'BufEnter' }, {
       callback = function()
         lint.try_lint()
       end,
