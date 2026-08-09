a2dcf3a599c7e6bddd441c01f9b30e248bfd12f9
Rex-Prime <rexdotprime@gmail.com>
2026-08-09 01:47:16 -0400

    fix(nvim): forgot to import config to lazy

--- dot_config/nvim/lua/config/lazy.lua
+++ dot_config/nvim/lua/config/lazy.lua
@@ -15,6 +15,9 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
+require('config.keymaps')
+require('config.options')
+
 -- Setup lazy.nvim
 require("lazy").setup({
   spec = {
